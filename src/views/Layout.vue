67afce18a3f5e0c74bf943b040d678d2f39f29ec
zhangxingri <zhangdou127@outlook.com>
2019-11-13 19:22:21 +0800

    初始化完成

---
+++ src/views/Layout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">Layout</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang='less'></style>
---
+++ src/views/user/Index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">user</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang='less'></style>
